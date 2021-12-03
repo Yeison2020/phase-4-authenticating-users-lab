824e0dc1b61f8078df33906123ff6d61ee4b6754
Yeison <enmanuelquiterios@gmail.com>
2021-12-03 09:41:26 -0500

    Reviewing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+
+
+    # Here is just the reload page to send back data to the current user, because react when refreshes trhe pages loses the state
+    # Why do we use sessions my undersatanding is to find the current user from the sessions using their cookies
     def show 
         user = User.find(session[:user_id])
         render json: user
